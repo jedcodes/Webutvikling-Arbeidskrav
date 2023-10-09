821f95bc1cfe250a4f456b3bfe52ac9cda653e33
Abdifatah Hassan <111760791+Abdifatah-Hassan@users.noreply.github.com>
2023-10-09 19:39:22 +0200

    Update ResourcesModule.js

--- arbeidskrav-23/scripts/modules/ResourcesModule.js
+++ arbeidskrav-23/scripts/modules/ResourcesModule.js
@@ -21,3 +21,56 @@ const ResourcesModule = (() => {
 
 export default ResourcesModule;
 
+/* Copy
+const resourcesModule = (()=>{
+
+
+    const resources =[
+        {
+                name: "Snake",
+                warriorPrice: 200,
+                image: "warrior-1.jpg",
+                coin: "gold-coin.png"}, 
+            {
+                name: "Giant",
+                warriorPrice: 500 ,
+                image: "warrior-2.jpg",
+                coin: "gold-coin.png"
+
+            },
+            {
+                name: "Big Axe",
+                warriorPrice: 150,
+                image: "warrior-3.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Thief",
+                warriorPrice: 50,
+                image: "warrior-4.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Tanks",
+                warriorPrice: 250,
+                image: "warrior-5.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Berserker",
+                warriorPrice: 275,
+                image: "warrior-6.jpg",
+                coin: "gold-coin.png"
+            },
+
+
+    ];
+
+    const getAll = () => structuredClone( resources  );
+    return{
+        getAll
+    }
+
+})();
+
+export default resourcesModule;
