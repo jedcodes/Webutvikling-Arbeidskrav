9e5d196fa39b97f4187ee7cde7dfec76a28eb12b
Abdifatah Hassan <111760791+Abdifatah-Hassan@users.noreply.github.com>
2023-10-09 18:51:07 +0200

    Update ResourcesModule.js

--- arbeidskrav-23/scripts/modules/ResourcesModule.js
+++ arbeidskrav-23/scripts/modules/ResourcesModule.js
@@ -20,3 +20,58 @@ const ResourcesModule = (() => {
 })();
 
 export default ResourcesModule;
+
+/* Dette er ResourcesModule jeg har lagd (Abdifatah)
+
+const ResourcesModule = (()=>{
+
+
+    const Resources =[
+        {
+                name: "Snake",
+                warriorPrice: 200,
+                image: "warrior-1.jpg",
+                coin: "gold-coin.png"}, 
+            {
+                name: "Giant",
+                warriorPrice: 500 ,
+                image: "warrior-2.jpg",
+                coin: "gold-coin.png"
+
+            },
+            {
+                name: "Big Axe",
+                warriorPrice: 150,
+                image: "warrior-3.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Thief",
+                warriorPrice: 50,
+                image: "warrior-4.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Tanks",
+                warriorPrice: 250,
+                image: "warrior-5.jpg",
+                coin: "gold-coin.png"
+            },
+            {
+                name: "Berserker",
+                warriorPrice: 275,
+                image: "warrior-6.jpg",
+                coin: "gold-coin.png"
+            },
+
+
+    ];
+
+    const getAll = () => structuredClone( Resources  );
+    return{
+        getAll
+    }
+
+})();
+
+export default ResourcesModule;
