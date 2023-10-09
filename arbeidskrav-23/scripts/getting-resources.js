097ca1f404fd7a888a394263ee8423ba522a9615
Abdifatah Hassan <111760791+Abdifatah-Hassan@users.noreply.github.com>
2023-10-09 19:38:32 +0200

    Update getting-resources.js

--- arbeidskrav-23/scripts/getting-resources.js
+++ arbeidskrav-23/scripts/getting-resources.js
@@ -1,1 +1,33 @@
+import resourcesModule from "./modules/resourcesModule.js";
 
+const outputSection = document.querySelector("#output-section");
+
+const showall = () => {
+
+    const allResources = resourcesModule.getAll();
+
+    let htmlText = "";
+
+    allResources.forEach( resource => {
+        htmlText += `
+        <article class="col-12 col-md-6 col-lg-4"> 
+        <div class="border shadow p-3 rounded h-100">
+        <div class="text-center "  style="width: 10.6rem;"
+                     <h3 class="">${resource.name}</h3>
+            <img clas="img-fluid img-fluid img-thumbnail " src="images/${resource.image}">
+           
+         <button class="btn btn-success "> BuyWarrior </button>
+             
+
+
+        </div>
+
+        </div>
+        </article>
+        `
+    });
+    outputSection.innerHTML = htmlText;
+}
+(()=>{
+    showall();
+})();
